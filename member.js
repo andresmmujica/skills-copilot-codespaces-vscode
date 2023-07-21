f5b08ac31894e4308932773fa6e67918851e27be
Andrés Mauricio Mujica Zalamea <andres.mujica@seaq.co>
2023-07-21 02:31:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    var member = document.getElementById('member');
+    member.innerHTML = `
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Member</h1>
+            </div>
+        </div>
+    </div>
+    `;
+}
